cb4240e6b4f139e828e0114a579b082a2e9bfe0e
joakin <joaquin@chimeces.com>
2021-03-01 01:04:51 +0100

    Build output, extract CSS to external, document commands to build

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # robot-simulation
 
+- `npm start` to compile Elm to JS
+- `npm run dev` to compile Elm to JS in debug mode
+- `npm test` to run the Elm tests
+- `npm run test-watch` to run the Elm tests in watch mode
+
 ## Code characteristics
 
 - Well-structured
@@ -12,14 +17,12 @@ Create a Javascript application in which you can simulate controlling a robot.
 The robot should be placed on a 5x5 grid. Supply controls that let you move the
 robot forwards in the direction it is facing, and rotate the robot in place to
 face any cardinal direction. Use any Javascript framework you’re comfortable
-with, as long as it runs in modern web browsers (we’re not looking for backwards
-compatibility in this test). Take a look around bellroy.com and use what you see
-to give your application a Bellroy “look”.
+with, as long as it runs in modern web browsers.
 
 Commit your code in a git repository and submit a tarball of that repository
 when you submit your application.
 
-## Style
+## Style hints
 
 - Fonts
   - Title font: PT Serif, sans-serif
--- src/Main.elm
+++ src/Main.elm
@@ -48,82 +48,11 @@ init () =
     ( { robot = Robot.make (Position 2 2) }, Cmd.none )
 
 
-css =
-    """
-@import url('https://fonts.googleapis.com/css2?family=Lato&family=PT+Serif&display=swap');
-
-:root {
-    --cell-size: 50px;
-    --light-bg: #E9E9E9;
-    --accent-bg: #EA6E2F;
-}
-
-body {
-    font-family: 'Lato', sans-serif;
-    font-size: 18px;
-    line-height: 1.6;
-    letter-spacing: 1.5px;
-    background-color: var(--light-bg);
-}
-
-body>* {
-    padding: 2em 20%;
-}
-
-h1 {
-    font-family: 'PT Serif', sans-serif;
-    letter-spacing: 1px;
-    background-color: white;
-    margin: 0;
-}
-
-.Banner {
-    background-color: var(--accent-bg);
-    color: white;
-    margin: 0;
-}
-
-.Game {
-    display: flex;
-    justify-content: center;
-}
-
-.Grid {
-    border: 1px solid var(--light-bg);
-    position: relative;
-}
-
-.Cell {
-    border: 1px solid var(--light-bg);
-    width: var(--cell-size);
-    height: var(--cell-size);
-    position: absolute;
-    background-color: white;
-    z-index: 1;
-}
-
-.Robot {
-    width: var(--cell-size);
-    height: var(--cell-size);
-    position: absolute;
-    left: 0;
-    top: 0;
-    font-size: calc(var(--cell-size)*0.75);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: transform 100ms ease-in;
-    z-index: 2;
-}
-"""
-
-
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Robot simulation!"
+    { title = "Robot simulation"
     , body =
-        [ Html.node "style" [] [ text css ]
-        , h1 [] [ text "Robot simulation!" ]
+        [ h1 [] [ text "Robot simulation" ]
         , section [ class "Banner" ]
             [ p [] [ text "Instructions:" ]
             , ul []
